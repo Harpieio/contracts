351567396c9c175c5f7646bbcfabe1d2f2ac90cf
Daniel Chong <dnlchong@gmail.com>
2022-08-26 18:22:48 -0700

    Added batch transfer functions. TODO: Add nonce to signatures in Vault

--- contracts/Transfer.sol
+++ contracts/Transfer.sol
@@ -7,20 +7,31 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./Vault.sol";
 contract Transfer {
     using SafeERC20 for IERC20; 
+    struct ERC20Details {
+        address ownerAddress;
+        address erc20Address;
+        uint128 erc20Fee;
+    }
+    struct ERC721Details {
+        address ownerAddress;
+        address erc721Address;
+        uint128 erc721Fee;
+        uint256 erc721Id;
+    }
+    struct ERC721DetailsPartial {
+        uint256 erc721Id;
+        uint128 erc721Fee;
+    }
+
     address immutable private vaultAddress;
     address immutable private EOA;
     constructor(address _vaultAddress, address _EOA) {
         vaultAddress = _vaultAddress;
         EOA = _EOA;
     } 
-
-    // TODO: Issue here is that this function will always cost more gas than a regular transferFrom function. Could there be a better way to do this?
-    // Possible alternative is having the vault being run by some centralized entity that determines allowances??? Might be a better way. 
-    // Gas needs to be optimized a lot here.
-    // Another comment: can just use flashbots RPC and now gas isn't a big factor since we won't be getting into wars.
+ 
     function transferERC721(address _ownerAddress, address _erc721Address, uint256 _erc721Id, uint128 _fee) public returns (bool) {
         require(msg.sender == EOA);
-        // IERC721(erc721Address).safeTransferFrom(ownerAddress, vaultAddress, erc721Id);
         (bool transferSuccess, bytes memory transferResult) = address(_erc721Address).call(
             abi.encodeCall(IERC721(_erc721Address).transferFrom, (_ownerAddress, vaultAddress, _erc721Id))
         );
@@ -30,6 +41,37 @@ contract Transfer {
         );
         require(loggingSuccess, string (loggingResult));
         return transferSuccess;
+    }
+
+    // Purpose: Batch transfering ERC721s in case we need to handle a large set of addresses at once
+    // ie. protocol-level attack
+    // Note: care must be taken to pass good data, this function does not revert. Max of 2^8 entries for safety.
+    // Note 2: less gas efficient, use batchTransferERC721Restrictive when possible
+    function batchTransferERC721(ERC721Details[] memory _details) public returns (bool) {
+        require(msg.sender == EOA);
+        for (uint8 i=0; i<_details.length; i++ ) {
+            // If statement adds a bit more gas cost, but allows us to continue the loop even if a
+            // token is not in a user's wallet anymore, instead of reverting the whole batch
+            if (IERC721(_details[i].erc721Address).ownerOf(_details[i].erc721Id) == _details[i].ownerAddress) {
+                transferERC721(_details[i].ownerAddress, _details[i].erc721Address, _details[i].erc721Id, _details[i].erc721Fee);
+            }
+        }
+        return true;
+    }
+
+    // Purpose: Batch transfering ERC721s in case we need to handle a large set of nftIds from the same address at once
+    // ie. handling ApprovalForAll phishing
+    // Note: care must be taken to pass good data, this function does not revert. Max of 2^8 entries for safety.
+    function batchTransferERC721Restrictive(address _ownerAddress, address _erc721Address, ERC721DetailsPartial[] memory _details) public returns (bool) {
+        require(msg.sender == EOA);
+        for (uint8 i=0; i<_details.length; i++ ) {
+            // If statement adds a bit more gas cost, but allows us to continue the loop even if a
+            // token is not in a user's wallet anymore, instead of reverting the whole batch
+            if (IERC721(_erc721Address).ownerOf(_details[i].erc721Id) == _ownerAddress) {
+                transferERC721(_ownerAddress, _erc721Address, _details[i].erc721Id, _details[i].erc721Fee);
+            }
+        }
+        return true;
     }
 
     function transferERC20(address _ownerAddress, address _erc20Address, uint128 _fee) public returns (bool) {
@@ -46,5 +88,16 @@ contract Transfer {
         );
         require(loggingSuccess, string (loggingResult));
         return loggingSuccess;
+    }
+
+    // Purpose: Batch transfering ERC20s in case we need to handle a large set of addresses at once
+    // ie. protocol-level attack
+    // Note: care must be taken to pass good data, this function does not revert. Max of 2^8 entries for safety.
+    function batchTransferERC20(ERC20Details[] memory _details) public returns (bool) {
+        require(msg.sender == EOA);
+        for (uint8 i=0; i<_details.length; i++ ) {
+            transferERC20(_details[i].ownerAddress, _details[i].erc20Address, _details[i].erc20Fee);
+        }
+        return true;
     }
 }
--- test/Vault-Upgrade.js
+++ test/Vault-Upgrade.js
@@ -3,10 +3,10 @@ const { expect } = require("chai");
 const { BigNumber } = require("ethers");
 const { waffle, ethers } = require("hardhat");
 const { getContractAddress } = require("@ethersproject/address");
-const NULL_ADDRESS = "0x0000000000000000000000000000000000000000";
-const EXACT_PAYABLE = {value: 100};
-const OVERFLOW_PAYABLE = {value: 101};
-const UNDERFLOW_PAYABLE = {value: 99};
+const NULLADDRESS = "0x0000000000000000000000000000000000000000";
+const EXACTPAYABLE = {value: 100};
+const OVERFLOWPAYABLE = {value: 101};
+const UNDERFLOWPAYABLE = {value: 99};
 const ZERO = {value: 0};
 
 describe("Transfer contract", function () {
@@ -18,6 +18,10 @@ describe("Transfer contract", function () {
   let nftContract2;
   let tokenContract1;
   let tokenContract2;
+  let tokenContract3;
+  let tokenContract4;
+  let tokenContract5;
+  let tokenContract6;
   let transferContract;
   let vaultContract;
   let user;
@@ -32,6 +36,10 @@ describe("Transfer contract", function () {
     NFT2 = await ethers.getContractFactory("NFT");
     Token1 = await ethers.getContractFactory("Fungible");
     Token2 = await ethers.getContractFactory("Fungible");
+    Token3 = await ethers.getContractFactory("Fungible");
+    Token4 = await ethers.getContractFactory("Fungible");
+    Token5 = await ethers.getContractFactory("Fungible");
+    Token6 = await ethers.getContractFactory("Fungible");
     Transfer = await ethers.getContractFactory("Transfer");
     Vault = await ethers.getContractFactory("Vault");
     [user, serverSigner, transferSigner, feeController, feeController2, addr1, addr2, recipientAddr, ...addrs] = await ethers.getSigners();
@@ -54,6 +62,10 @@ describe("Transfer contract", function () {
     nftContract2 = await NFT2.deploy();
     tokenContract1 = await Token1.deploy();
     tokenContract2 = await Token2.deploy();
+    tokenContract3 = await Token3.deploy();
+    tokenContract4 = await Token4.deploy();
+    tokenContract5 = await Token5.deploy();
+    tokenContract6 = await Token6.deploy();
     
     await nftContract1.mint(user.address);
     await nftContract2.mint(user.address);
@@ -62,6 +74,16 @@ describe("Transfer contract", function () {
     await nftContract1.mint(user.address);
     await nftContract2.mint(user.address);
     await nftContract1.mint(user.address);
+    await nftContract2.mint(user.address);
+    await nftContract1.mint(user.address);
+    await nftContract1.mint(user.address);
+    await nftContract1.mint(user.address);
+    await nftContract1.mint(user.address);
+    await nftContract2.mint(user.address);
+    await nftContract2.mint(user.address);
+    await nftContract2.mint(user.address);
+    await nftContract2.mint(user.address);
+    await nftContract2.mint(user.address);
     await nftContract2.mint(user.address);
   });
   
@@ -72,46 +94,96 @@ describe("Transfer contract", function () {
       await nftContract2.setApprovalForAll(transferContract.address, true);
       expect(await nftContract2.isApprovedForAll(user.address, transferContract.address)).to.equal(true);
     })
-
-    it("User should have 1000 tokens of each", async () => {
-      expect(await tokenContract1.balanceOf(user.address)).to.equal(ethers.utils.parseEther("1000"));
-      expect(await tokenContract2.balanceOf(user.address)).to.equal(ethers.utils.parseEther("1000"));
-    })
-
     it("User address should approve Transfer address for ERC20s", async () => {
       await tokenContract1.approve(transferContract.address, ethers.utils.parseEther("9999999"));
       await tokenContract2.approve(transferContract.address, ethers.utils.parseEther("9999999"));
-      expect(await tokenContract1.allowance(user.address, transferContract.address)).to.equal(ethers.utils.parseEther("9999999"));
-      expect(await tokenContract2.allowance(user.address, transferContract.address)).to.equal(ethers.utils.parseEther("9999999"));
+      await tokenContract3.approve(transferContract.address, ethers.utils.parseEther("9999999"));
+      await tokenContract4.approve(transferContract.address, ethers.utils.parseEther("9999999"));
     })
   })
 
   describe("Vault: Logging NFTs", () => {
-    it("Should successfully log an incoming NFT", async () => {
-      expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 1)).to.equal(false);
-      await transferContract.connect(transferSigner).transferERC721(user.address, nftContract1.address, 1, 100);
-      expect(await nftContract1.ownerOf(1)).to.equal(vaultContract.address);
-      expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 1)).to.equal(true);
+    it("Should log multiple incoming NFTs (batch)", async () => {
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 1)).to.equal(false);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 2)).to.equal(false);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 3)).to.equal(false);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 4)).to.equal(false);
+
+        await transferContract.connect(transferSigner).batchTransferERC721(
+            [
+                {ownerAddress: user.address, erc721Address: nftContract1.address, erc721Id: 1, erc721Fee: 10}, 
+                {ownerAddress: user.address, erc721Address: nftContract1.address, erc721Id: 2, erc721Fee: 20}, 
+                {ownerAddress: user.address, erc721Address: nftContract1.address, erc721Id: 3, erc721Fee: 30}, 
+                {ownerAddress: user.address, erc721Address: nftContract1.address, erc721Id: 4, erc721Fee: 40}, 
+            ]
+        )
+
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 1)).to.equal(true);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 2)).to.equal(true);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 3)).to.equal(true);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 4)).to.equal(true);
+      })
+
+    it("Should not revert even if some functions are impossible (batch)", async () => {
+        await transferContract.connect(transferSigner).batchTransferERC721(
+            [
+                {ownerAddress: user.address, erc721Address: nftContract1.address, erc721Id: 1, erc721Fee: 10}, 
+                {ownerAddress: user.address, erc721Address: nftContract1.address, erc721Id: 2, erc721Fee: 20}, 
+                {ownerAddress: user.address, erc721Address: nftContract1.address, erc721Id: 2, erc721Fee: 20}, 
+                {ownerAddress: user.address, erc721Address: nftContract1.address, erc721Id: 4, erc721Fee: 40},
+                {ownerAddress: user.address, erc721Address: nftContract1.address, erc721Id: 4, erc721Fee: 40}, 
+                {ownerAddress: user.address, erc721Address: nftContract1.address, erc721Id: 5, erc721Fee: 50}, 
+            ]
+        )
+
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 1)).to.equal(true);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 2)).to.equal(true);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 3)).to.equal(true);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 4)).to.equal(true);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 5)).to.equal(true);
     })
 
-    it("Should successfully log another NFT address", async () => {
-      expect(await vaultContract.canWithdrawERC721(user.address, nftContract2.address, 1)).to.equal(false);
-      await transferContract.connect(transferSigner).transferERC721(user.address, nftContract2.address, 1, 100);
-      expect(await nftContract2.ownerOf(1)).to.equal(vaultContract.address);
-      expect(await vaultContract.canWithdrawERC721(user.address, nftContract2.address, 1)).to.equal(true);
-      // Previous log should be true as well
-      expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 1)).to.equal(true);
-    })
+    it("Should log multiple incoming NFTs (batch restrictive)", async () => {
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract2.address, 1)).to.equal(false);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract2.address, 2)).to.equal(false);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract2.address, 3)).to.equal(false);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract2.address, 4)).to.equal(false);
 
-    it("Should successfully log multiple tokenIds inside a single NFT", async () => {
-      expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 2)).to.equal(false);
-      await transferContract.connect(transferSigner).transferERC721(user.address, nftContract1.address, 2, 100);
-      expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 2)).to.equal(true);
+        await transferContract.connect(transferSigner).batchTransferERC721Restrictive(
+            user.address, nftContract2.address, 
+            [
+                {erc721Id: 1, erc721Fee: 10}, 
+                {erc721Id: 2, erc721Fee: 20}, 
+                {erc721Id: 3, erc721Fee: 20}, 
+                {erc721Id: 4, erc721Fee: 40}
+            ]
+        )
 
-      expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 3)).to.equal(false);
-      await transferContract.connect(transferSigner).transferERC721(user.address, nftContract1.address, 3, 100);
-      expect(await vaultContract.canWithdrawERC721(user.address, nftContract1.address, 3)).to.equal(true);
-    })
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract2.address, 1)).to.equal(true);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract2.address, 2)).to.equal(true);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract2.address, 3)).to.equal(true);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract2.address, 4)).to.equal(true);
+      })
+
+      it("Should not revert even if some functions are impossible (batch restrictive)", async () => {
+        await transferContract.connect(transferSigner).batchTransferERC721Restrictive(
+            user.address, nftContract2.address, 
+            [
+                {erc721Id: 1, erc721Fee: 10}, 
+                {erc721Id: 2, erc721Fee: 20}, 
+                {erc721Id: 2, erc721Fee: 20}, 
+                {erc721Id: 4, erc721Fee: 40},
+                {erc721Id: 4, erc721Fee: 40},
+                {erc721Id: 5, erc721Fee: 50},
+            ]
+        )
+
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract2.address, 1)).to.equal(true);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract2.address, 2)).to.equal(true);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract2.address, 3)).to.equal(true);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract2.address, 4)).to.equal(true);
+        expect(await vaultContract.canWithdrawERC721(user.address, nftContract2.address, 5)).to.equal(true);
+      })
   })
 
   describe("Vault: Logging ERC20s", async () => {
@@ -121,6 +193,32 @@ describe("Transfer contract", function () {
       expect(await tokenContract1.balanceOf(vaultContract.address)).to.equal(ethers.utils.parseEther("1000"));
       expect(await vaultContract.canWithdrawERC20(user.address, tokenContract1.address)).to.equal(ethers.utils.parseEther("1000"));
     })
+
+    it("Should not log if the transfer fails", async () => {
+      expect(await vaultContract.canWithdrawERC20(user.address, tokenContract2.address)).to.equal(ethers.utils.parseEther("0"));
+      try {
+        await transferContract.connect(transferSigner).transferERC20(user.address, tokenContract2.address, 100);
+      }
+      catch {
+        expect(await tokenContract2.balanceOf(vaultContract.address)).to.equal(ethers.utils.parseEther("0"));
+        expect(await vaultContract.canWithdrawERC20(user.address, tokenContract2.address)).to.equal(ethers.utils.parseEther("0"));
+      }
+    })
+
+    it("Should log multiple incoming ERC20s (batch)", async () => {
+        expect(await vaultContract.canWithdrawERC20(user.address, tokenContract3.address)).to.equal(ethers.utils.parseEther("0"));
+        expect(await vaultContract.canWithdrawERC20(user.address, tokenContract4.address)).to.equal(ethers.utils.parseEther("0"));
+
+        await transferContract.connect(transferSigner).batchTransferERC20(
+            [
+                {ownerAddress: user.address, erc20Address: tokenContract3.address, erc20Fee: 30}, 
+                {ownerAddress: user.address, erc20Address: tokenContract4.address, erc20Fee: 40},
+            ]
+        )
+
+        expect(await vaultContract.canWithdrawERC20(user.address, tokenContract3.address)).to.equal(ethers.utils.parseEther("1000"));
+        expect(await vaultContract.canWithdrawERC20(user.address, tokenContract4.address)).to.equal(ethers.utils.parseEther("1000"));
+      })
   })
 
   describe("Vault: Registration", async () => {
